267bd1a6fa1c3220d41803eb2df07f82e62df41a
Vinay Chandola <vinaychandola09042002@gmail.com>
2022-11-20 00:13:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// Add Authencation
+
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// connecting with mongodb
+mongoose.connect("mongodb://127.0.0.1/NameDB", { useNewUrlParser: true });
+
+const NamesSchema = {
+  name: String,
+};
+const Name = mongoose.model("Name", NamesSchema);
+
+app.route("/names").get(function (req, res) {
+  //reading data from the database;
+  Name.find({}, function (err, found) {
+    if (err) res.send(err);
+    else {
+      res.send(found);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server Started on port 3000");
+});
