0443f18818e26539e1e16e95c8ea202dbeac09cf
Vinay Chandola <72332260+Hero-debug-cloud@users.noreply.github.com>
2022-11-20 12:40:43 +0530

    updating

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.route("/names").get(function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server Started on port 3000");
-});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
