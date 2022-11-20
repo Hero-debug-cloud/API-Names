bd75d8a4ce9a8710b7a6e75dec6556339d39d0cb
Vinay Chandola <vinaychandola09042002@gmail.com>
2022-11-20 19:20:33 +0530

    adding for things like put delete post

--- server.js
+++ server.js
@@ -23,16 +23,96 @@ const NamesSchema = {
 };
 const Name = mongoose.model("Name", NamesSchema);
 
-app.route("/names").get(function (req, res) {
-  //reading data from the database;
-  Name.find({}, function (err, found) {
-    if (err) res.send(err);
-    else {
-      res.send(found);
-    }
-  });
-});
+///////////////// General Route for the API/////////////////////
+app
+  .route("/names")
 
-app.listen(3000, function () {
-  console.log("Server Started on port 3000");
-});
+  // Sending data to the api
+  .get(function (req, res) {
+    //reading data from the database;
+    Name.find({}, function (err, found) {
+      if (err) res.send(err);
+      else {
+        res.send(found);
+      }
+    });
+  })
+
+  //updating our database using api calls;
+  .post(function (req, res) {
+    //storing updated values in a variable;
+    const name = req.body.name;
+    const value = new Name({
+      name: name,
+    });
+    value.save(function (err) {
+      if (err) res.send(err);
+      else res.send("Value Inserted successfully in the database");
+    });
+  })
+
+  //deleting value from our database using api calls;
+  .delete(function (req, res) {
+    //storing deleting value;
+    const value = req.body.name;
+    Name.deleteOne({ name: value }, function (err) {
+      if (err) res.send(err);
+      else res.send("Value deleted successfully in the database");
+    });
+  })
+
+  //updating value from the database;
+  .put(function (req, res) {
+    const id = req.body.id;
+    const value = req.body.name;
+    Name.updateOne({ _id: id }, { name: value }, function (err) {
+      if (err) res.send(err);
+      else res.send("Value Updated successfully in the database");
+    });
+  });
+
+///////////////////////  For Specific Routes///////////////////////////////
+
+app
+  .route("/names/:name")
+
+  .get(function (req, res) {
+    const value = req.params.name;
+    Name.findOne({ name: value }, function (err, found) {
+      if (err) res.send(err);
+      else if (found == null) res.send("Value not found");
+      else res.send(found);
+    });
+  })
+
+  .post(function (req, res) {
+    const name = req.params.name;
+    const value = new Name({
+      name: name,
+    });
+    value.save(function (err) {
+      if (err) res.send(err);
+      else res.send("Value inserted successfully in the database");
+    });
+  })
+
+  .delete(function (req, res) {
+    const name = req.params.name;
+    Name.deleteOne({ name: name }, function (err) {
+      if (err) res.send(err);
+      else res.send("Value deleted successfully");
+    });
+  })
+
+  .put(function (req, res) {
+    const value = req.params.name;
+    const id = req.body.id;
+    Name.updateOne({ _id: id }, { name: value }, function (err) {
+      if (err) res.send(err);
+      else res.send("Value updated successfully in the database");
+    });
+  });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
